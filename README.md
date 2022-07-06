4c8d1616b1dafe8129adfcd6c7922bb322e98005
andy-maca <andy.ca.ma@foxmail.com>
2022-07-06 10:05:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-vqdej7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-vqdej7)
---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  VERSION,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Right click on the black rectangle to show context menu';
+  show = false;
+  delayStarted = false;
+  private timerId = 0;
+
+  @HostListener('window:click', ['$event'])
+  onclick(e: MouseEvent) {
+    if (e.target !== this.menuElement.nativeElement) {
+      this.show = false;
+    }
+  }
+
+  @ViewChild('contextMenu', { static: true })
+  menuElement: ElementRef<HTMLElement>;
+
+  showMenu(e: MouseEvent, delay) {
+    this.show = false;
+    e.preventDefault();
+    this.menuElement.nativeElement.style.left = e.pageX + 'px';
+    this.menuElement.nativeElement.style.top = e.pageY + 'px';
+    clearTimeout(this.timerId);
+
+    this.delayStarted = true;
+    this.timerId = setTimeout(() => {
+      this.show = true;
+      this.delayStarted = false;
+    }, delay);
+  }
+}
